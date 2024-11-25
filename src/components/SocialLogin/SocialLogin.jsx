63968194a4bc825f927bd636ac90115ca0f100ef
asifalam515 <asibulalam515@gmail.com>
2024-11-25 08:40:42 +0600

    google login applied

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,14 +1,26 @@
 import { FaGoogle } from "react-icons/fa";
 import useAuth from "../../hooks/useAuth";
 import Swal from "sweetalert2";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import { useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
+  const axiosPublic = useAxiosPublic();
   const { googleSignIn } = useAuth();
+  const navigate = useNavigate();
   const handleGoogleSignIn = () => {
     googleSignIn().then((result) => {
       const user = result.user;
-      console.log(user);
-      Swal.fire("Logged In Using google");
+      const userInfo = {
+        email: result.user?.email,
+        name: result.user?.displayName,
+      };
+      axiosPublic.post("/users", userInfo).then((res) => {
+        console.log(res.data);
+        Swal.fire("Logged In Using google");
+        navigate("/");
+      });
     });
   };
   return (
