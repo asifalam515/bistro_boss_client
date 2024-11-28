0d1ed1f3e8e17dd4d071b9fc15e78582f23c6dae
asifalam515 <asibulalam515@gmail.com>
2024-11-28 14:44:08 +0600

    started with react form hooks

--- src/pages/DashBoard/AddItems/AddItems.jsx
+++ src/pages/DashBoard/AddItems/AddItems.jsx
@@ -1,12 +1,23 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
-
+import { useForm } from "react-hook-form";
 const AddItems = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
     <div>
       <SectionTitle
         heading="add an item"
         subHeading="Whats new?"
       ></SectionTitle>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("firstName")} />
+        <select {...register("gender")}>
+          <option value="female">female</option>
+          <option value="male">male</option>
+          <option value="other">other</option>
+        </select>
+        <input type="submit" />
+      </form>
     </div>
   );
 };
