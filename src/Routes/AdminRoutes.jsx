ba57519cad97aa4d8e189d318487a8a89cb76804
asifalam515 <asibulalam515@gmail.com>
2024-11-28 11:52:34 +0600

    redone

--- src/Routes/AdminRoutes.jsx
+++ src/Routes/AdminRoutes.jsx
@@ -1,20 +1,18 @@
 import { Navigate, useLocation } from "react-router-dom";
-import useAdmin from "../hooks/useAdmin";
 import useAuth from "../hooks/useAuth";
+import useAdmin from "../hooks/useAdmin";
 
 const AdminRoutes = ({ children }) => {
-  const [user, loading] = useAuth();
-  const [isAdmin, isAdminLoading] = useAdmin();
   const location = useLocation();
+  const { user, loading } = useAuth();
+  const [isAdmin, isAdminLoading] = useAdmin();
   if (loading || isAdminLoading) {
     return <progress className="progress w-56"></progress>;
   }
-
   if (user && isAdmin) {
     return children;
   }
-
-  return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+  return <Navigate to="/" state={{ form: location }} replace></Navigate>;
 };
 
 export default AdminRoutes;
