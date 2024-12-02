c500c9d8844900b0ece9d33d5d2714fb7e8903dc
asifalam515 <asibulalam515@gmail.com>
2024-12-02 10:24:11 +0600

    checkout form

--- src/pages/DashBoard/Payment/CheckoutForm.jsx
+++ src/pages/DashBoard/Payment/CheckoutForm.jsx
@@ -5,6 +5,13 @@ const CheckoutForm = () => {
   const elements = useElements();
   const handleSubmit = async (event) => {
     event.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+    const card = elements.getElement(CardElement);
+    if (card == null) {
+      return;
+    }
   };
   return (
     <div>
