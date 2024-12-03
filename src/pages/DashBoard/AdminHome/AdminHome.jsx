f1e435b0366f3c62334382ce6d0272fb4c8c94a1
asifalam515 <asibulalam515@gmail.com>
2024-12-03 11:35:00 +0600

    admin home creted

--- src/pages/DashBoard/AdminHome/AdminHome.jsx
+++ src/pages/DashBoard/AdminHome/AdminHome.jsx
@@ -1,13 +1,67 @@
+import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import {
+  FaBook,
+  FaDollarSign,
+  FaJediOrder,
+  FaProductHunt,
+  FaUsers,
+} from "react-icons/fa";
 
 const AdminHome = () => {
   const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: stats } = useQuery({
+    queryKey: ["admin-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/admin-stats");
+      return res.data;
+    },
+  });
   return (
     <div>
       <h2 className="text-3xl">
         <span>Hi,Welcome </span>
         {user?.displayName ? user.displayName : "Back"}
       </h2>
+      <div className="stats shadow">
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaDollarSign className="text-4xl"></FaDollarSign>
+          </div>
+          <div className="stat-title">Revenue</div>
+          <div className="stat-value">$ {stats.revenue} </div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaUsers className="text-3xl"></FaUsers>
+          </div>
+          <div className="stat-title">Users</div>
+          <div className="stat-value"> {stats.users} </div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaBook></FaBook>
+          </div>
+          <div className="stat-title">Orders</div>
+          <div className="stat-value"> {stats.orders} </div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            {" "}
+            <FaJediOrder></FaJediOrder>
+          </div>
+          <div className="stat-title">Menu Items</div>
+          <div className="stat-value"> {stats.menuItem} </div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+      </div>
     </div>
   );
 };
